b342d55b61d3c584db3a96965501dc2eabd7b376
Andrija <akinovak@gmail.com>
2022-11-25 02:42:49 +0100

    add verifier structure

--- src/caulk_plus/verifier.rs
+++ src/caulk_plus/verifier.rs
@@ -1,5 +1,10 @@
+use std::marker::PhantomData;
+
+use ark_ec::PairingEngine;
 use ark_ff::Field;
 use ark_std::rand::RngCore;
+
+use super::{PublicInput, CommonInput, proof::Proof};
 
 pub struct VerifierMessages<F: Field> {
     pub xi_1: Option<F>,
@@ -22,5 +27,24 @@ impl<F: Field> VerifierMessages<F> {
 
     pub fn receive_second_msg<R: RngCore>(&mut self, rng: &mut R) {
         self.alpha = Some(F::rand(rng));
+    }
+}
+
+pub struct Verifier<E: PairingEngine> {
+    _e: PhantomData<E>
+}
+
+impl<E: PairingEngine> Verifier<E> {
+    // TODO: return result from here
+    pub fn verify(
+        public_input: &PublicInput<E>, 
+        common_input: &CommonInput<E>, 
+        proof: &Proof<E>
+    ) {
+        // 1. compute p1
+
+        // 2. compute p2
+
+        // 3. check openings
     }
 }
