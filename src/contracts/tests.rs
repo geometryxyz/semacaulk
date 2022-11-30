04ef9ebecbbc9c17ab42c506329f3d76e02f30f8
Koh Wei Jie <contact@kohweijie.com>
2022-12-01 00:58:50 +0800

    use semaphore-rs merkle tree impl

--- src/contracts/tests.rs
+++ src/contracts/tests.rs
@@ -9,7 +9,23 @@ use hex::decode;
 use ethers::{prelude::*, utils::Anvil};
 // use eyre::Result;
 use std::{convert::TryFrom, sync::Arc, time::Duration};
+use semaphore::merkle_tree::Branch;
+use crate::keccak_tree::{
+    KeccakTree,
+    KeccakMerkleProof,
+};
 
+fn flatten_proof(proof: &KeccakMerkleProof) -> Vec<[u8; 32]> {
+    let mut result = Vec::with_capacity(proof.0.len());
+    for branch in &proof.0 {
+        let hash = match branch {
+            Branch::Left(hash) => hash,
+            Branch::Right(hash) => hash,
+        };
+        result.push(hash.clone());
+    }
+    result
+}
 
 #[test]
 pub async fn test_keccak_256() {
@@ -29,6 +45,7 @@ pub async fn test_keccak_256() {
 pub async fn test_keccak_mt() {
     abigen!(KeccackMT, "./src/contracts/out/KeccakMT.sol/KeccakMT.json",);
     use keccack_mt::KeccackMT;
+    use ethers::core::types::U256;
 
     // 1. compile the contract (note this requires that you are inside the `examples` directory) and
     // launch anvil
@@ -47,26 +64,23 @@ pub async fn test_keccak_mt() {
     // 5. deploy contract
     let keccak_mt_contract = KeccackMT::deploy(client, ()).unwrap().send().await.unwrap();;
 
-    //let preimage = [0u8; 64];
-    //let r1 = keccak256(preimage);
-    let r1_vec = hex::decode("ad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5").unwrap();
-    let r2_vec = hex::decode("b4c11951957c6f8f642c4af61cd6b24640fec6dc7fc607ee8206a99e92410d30").unwrap();
+    let mut tree = KeccakTree::new(4, [0; 32]);
 
-    let r1: [u8; 32] = r1_vec.try_into().unwrap();
-    let r2: [u8; 32] = r2_vec.try_into().unwrap();
+    for index in 0..tree.num_leaves() {
+        let mut leaf = [0u8; 32];
+        leaf[31] = index as u8;
+        tree.set(index, leaf);
+    }
 
-    let zero = [0u8; 32];
-    let proof = vec![zero, r1];
+    for index in 0..tree.num_leaves() {
+        let proof = tree.proof(index).unwrap();
+        let flattened_proof = flatten_proof(&proof);
 
-    // 6. call contract function
-    let index = ethers::core::types::U256::from(1u64);
-    let result = keccak_mt_contract.gen_root_from_path(index, zero, proof).call().await.unwrap();
-    assert_eq!(hex::encode(r2), hex::encode(result));
+        let leaf = tree.leaves()[index];
+
+        // 6. call contract function
+        let index = U256::from(index);
+        let result = keccak_mt_contract.gen_root_from_path(index, leaf, flattened_proof).call().await.unwrap();
+        assert_eq!(hex::encode(tree.root()), hex::encode(result));
+    }
 }
-
-/*
- * r1 = keccak256([0, 0]) = ad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5
- * r2 = keccak256([r1, r1]) = b4c11951957c6f8f642c4af61cd6b24640fec6dc7fc607ee8206a99e92410d30
- * r3 = keccak256([r2, r2]) = 21ddb9a356815c3fac1026b6dec5df3124afbadb485c9ba5a3e3398a04b7ba85
- * r4 = keccak256([r3, r3]) = e58769b32a1beaf1ea27375a44095a0d1fb664ce2dd358e7fcbfb78c26a19344
- */
